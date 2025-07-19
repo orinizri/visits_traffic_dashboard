c3d8b6fdf9b376cc7e93eceaf16ca2f90f4ec158
oriDEV <ori.nizri.tal@gmail.com>
2025-07-19 23:08:03 +0300

    Lint import without use

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import DashboardPage from "./pages/Dashboard";
 import LoginPage from "./pages/Login";
